06636723e32d287e3cdc621b32971feba5640c9a
m0rv1nd <168345833+m0rv1nd@users.noreply.github.com>
2024-11-18 17:03:56 +0100

    02_repeatString done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let concatString = "";
+    if (num < 0) return "ERROR";
+    for (let i = 0; i < num; i++) {
+        concatString += string;
+    }
+    return concatString; 
 };
 
 // Do not edit below this line
