9e0e7c614121749783d9167ba3539b7897a7ff3d
m0rv1nd <168345833+m0rv1nd@users.noreply.github.com>
2024-11-22 10:13:01 +0100

    05_sumAll done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function(firstInteger, secondInteger) {
+    let sum = 0;
+    if (Number.isInteger(firstInteger) && firstInteger > 0 && 
+    Number.isInteger(secondInteger) && secondInteger > 0) {
+        if (firstInteger <= secondInteger) {
+            do {
+                sum += firstInteger;
+                firstInteger++;
+            } while (firstInteger <= secondInteger)
+            return sum;
+        } else {
+            do {
+                sum += secondInteger;
+                secondInteger++;
+            } while (secondInteger <= firstInteger)
+            return sum;
+        }
+    } 
+    else {
+        return "ERROR"
+    }
 };
 
+sumAll(4, 1)
+// Number.isInteger(10);
 // Do not edit below this line
 module.exports = sumAll;
